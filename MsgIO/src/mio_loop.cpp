33d185e011ee86056a106f60685eb7379a97b4e3
wsxiaoys <wsxiaoys.lh@gmail.com>
2010-12-14 20:42:03 +0800

    remove mio.cpp

--- MsgIO/src/mio_loop.cpp
+++ MsgIO/src/mio_loop.cpp
@@ -208,4 +208,54 @@ void loop_impl::worker_main()
         (*task)();
     }
 }
+
+loop::loop()
+{
+    _impl = new loop_impl();
+}
+
+loop::~loop()
+{
+    delete IMPL;
+}
+
+void loop::start(int num)
+{
+    IMPL->start(num);
+}
+
+void loop::run(int num)
+{
+    IMPL->run(num);
+}
+
+mio::shared_handler loop::add_handler_impl( shared_handler sh )
+{
+    return IMPL->add_handler_impl(sh);
+}
+
+void loop::remove_handler( int fd )
+{
+    return IMPL->remove_handler(fd);
+}
+
+void loop::submit_impl( task_t f )
+{
+    IMPL->submit_impl(f);
+}
+
+void loop::end()
+{
+    IMPL->end();
+}
+
+bool loop::is_end() const
+{
+    return IMPL->is_end();
+}
+
+void loop::join()
+{
+    IMPL->join();
+}
 } //namespace mio
